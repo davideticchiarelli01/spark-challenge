53829986e28208709f1014f05db5b3107352f174
davideticchiarelli01 <davideticchiarelli01@gmail.com>
2025-03-08 14:33:14 +0100

    second commit

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Creazione del contesto Spark e della sessione Spark
+sc = SparkContext.getOrCreate()
+spark = SparkSession(sc)
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')  # Messaggio di benvenuto
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    text = sc.textFile('file:///home/user/Downloads/romeo_and_juliet.txt')
+    text.collect()
+    
+    # Chiude il contesto Spark
+    spark.stop()
