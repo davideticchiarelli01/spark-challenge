088c2107b5bf890008673c14577a09b0f65ee6ac
niccolociotti <niccolociotti01@gmail.com>
2025-03-09 16:01:30 +0100

    linting

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from pyspark.sql.window import Window
 from pyspark.sql import functions as F
 from functools import reduce
 import time
-
 import csv
 import os
 
@@ -13,18 +12,16 @@ import os
 sc = SparkContext.getOrCreate()
 spark = SparkSession(sc)
 
-
 # Entry point for the data files
 ENTRY_POINT = "file:///home/user/Downloads/BDAchallenge2425"
 
+
 def read_csv(entry_point):
-    # Load paths of files
+    """Read CSV files and process them into a unified DataFrame."""
     file_paths = spark.sparkContext.wholeTextFiles(entry_point + "/*/*.csv")
     df_list = []
 
-    # Iterate over files
     for file_path, _ in file_paths.toLocalIterator():
-        # Load the data into a DataFrame
         df = (
             spark.read.option("header", "true")
             .csv(file_path)
@@ -32,47 +29,41 @@ def read_csv(entry_point):
             .withColumn("YEAR", F.regexp_extract(F.input_file_name(), "/(\d{4})/", 1))
             .select("STATION", "YEAR", "LATITUDE", "LONGITUDE", "TMP", "WND", "REM")
         )
-        #Add the DataFrame into a list
         df_list.append(df)
 
-    # Merge all the Dataframes from the list
+    # Merge all the DataFrames from the list
     final_df = reduce(lambda df1, df2: df1.unionByName(df2, allowMissingColumns=True), df_list)
-
-    # Read all CSV files under the given directory path
-    # df = (
-    #     spark.read.option("header", "true")  # Read with headers
-    #     .csv(entry_point + "/*/*.csv")  # Read all CSV files recursively
-    #     .withColumn("STATION",
-    #                 regexp_extract(input_file_name(), "[^/]+(?=\.csv)", 0))  # Extract station name from filename
-    #     .withColumn("YEAR", regexp_extract(input_file_name(), "/(\d{4})/", 1))  # Extract the year from the file path
-    # )
 
     return final_df
 
 
 def write_result(df):
+    """Display the result."""
     df.show()
 
-# Task 1 - placeholder function
-def task1(df):
 
-    result = df.filter(
-                (df['LATITUDE'] >= 30) & (df['LATITUDE'] <= 60) &
-                (df['LONGITUDE'] >= -135) & (df['LONGITUDE'] <= -90)) \
-            .withColumn(
-                'TMP', bround(split(df['TMP'], ',')[0].cast('decimal(10, 1)') / 10, 1)) \
-            .groupBy('TMP') \
-            .agg(count('*').alias('OCC')) \
-            .orderBy('OCC', ascending=False) \
-            .withColumn('COORDS', lit('[(60,-135);(30,-90)]')) \
-            .limit(10) \
-            .select('COORDS', 'TMP', 'OCC')
+def task1(df):
+    """Task 1: Filter by coordinates, process TMP column, and show the top 10 results."""
+    result = (
+        df.filter(
+            (df["LATITUDE"] >= 30) & (df["LATITUDE"] <= 60) &
+            (df["LONGITUDE"] >= -135) & (df["LONGITUDE"] <= -90)
+        )
+        .withColumn(
+            "TMP", bround(split(df["TMP"], ",")[0].cast("decimal(10, 1)") / 10, 1)
+        )
+        .groupBy("TMP")
+        .agg(count("*").alias("OCC"))
+        .orderBy("OCC", ascending=False)
+        .withColumn("COORDS", lit("[(60,-135);(30,-90)]"))
+        .limit(10)
+        .select("COORDS", "TMP", "OCC")
+    )
 
     write_result(result)
 
     # Save the result into CVS file
-    # output_path = "file:///home/user/Downloads/task1.csv"  # Modifica il percorso del file di output
-    ## Scrivi il DataFrame in un file CSV senza sovrascrivere
+    # output_path = "file:///home/user/Downloads/task1.csv"
     # try:
     #    result.select("COORDS", "TMP", "OCC") \
     #        .coalesce(1) \
@@ -83,25 +74,24 @@ def task1(df):
     #    print(f"Errore nel salvataggio del file: {e}")
 
 
-
-
-# Task 2 - placeholder function
 def task2(df):
-    windowSpec = Window.partitionBy("SPEED").orderBy(F.desc("OCC"))
+    """Task 2: Find the most frequent station per wind speed."""
+    window_spec = Window.partitionBy("SPEED").orderBy(F.desc("OCC"))
 
-    grouped = df.withColumn("SPEED", split(df["WND"], ",").getItem(1)) \
-        .groupBy("SPEED", "STATION") \
-        .agg(count("*").alias("OCC")) \
-        .withColumn("rank", F.rank().over(windowSpec)) \
-        .filter(F.col("rank") == 1) \
-        .drop("rank") \
+    grouped = (
+        df.withColumn("SPEED", split(df["WND"], ",").getItem(1))
+        .groupBy("SPEED", "STATION")
+        .agg(count("*").alias("OCC"))
+        .withColumn("rank", F.rank().over(window_spec))
+        .filter(F.col("rank") == 1)
+        .drop("rank")
         .orderBy("SPEED")
+    )
 
     write_result(grouped)
 
     # Save the result into CVS file
-    # output_path = "file:///home/user/Downloads/task2.csv"  # Modifica il percorso del file di output
-    ## Scrivi il DataFrame in un file CSV senza sovrascrivere
+    # output_path = "file:///home/user/Downloads/task2.csv"
     # try:
     #    grouped.select("SPEED", "STATION", "OCC") \
     #        .coalesce(1) \
@@ -111,14 +101,15 @@ def task2(df):
     # except Exception as e:
     #    print(f"Errore nel salvataggio del file: {e}")
 
-
+    # Old code
     # df.withColumn("SPEED", split(df["WND"], ",").getItem(1)) \
     #     .groupBy("SPEED", "STATION") \
     #     .agg(count("*").alias("OCC")) \
     #     .orderBy("SPEED").show()
 
-# Task 3 - placeholder function
+
 def task3(df):
+    """Task 3: Process precipitation values and calculate the average per station per year."""
     df = df.withColumn(
         "precipitation_values",
         F.regexp_extract(df["REM"], r"HOURLY INCREMENTAL PRECIPITATION VALUES \(IN\):(.*)", 1)
@@ -149,40 +140,29 @@ def task3(df):
         F.avg("Average").alias("avg_precipitation")
     )
 
-    # Step 2: Ordina per anno e precipitazione media in ordine crescente
     df_ordinato = df_media_precipitazioni.orderBy("YEAR", "avg_precipitation")
 
-    # Step 3: Usa le funzioni di finestra per ottenere le prime 10 stazioni per ogni anno
-    finestraSpec = Window.partitionBy("YEAR").orderBy("avg_precipitation")
+    finestra_spec = Window.partitionBy("YEAR").orderBy("avg_precipitation")
 
-    df_top_10_stazioni = df_ordinato.withColumn("rank", F.row_number().over(finestraSpec)) \
-        .filter(F.col("rank") <= 10)  # Seleziona solo le prime 10 stazioni per anno
-
-    df_top_10_stazioni = df_top_10_stazioni.drop("rank")
+    df_top_10_stazioni = (
+        df_ordinato.withColumn("rank", F.row_number().over(finestra_spec))
+        .filter(F.col("rank") <= 10)
+        .drop("rank")
+    )
 
     write_result(df_top_10_stazioni)
 
-    # Save the result into CVS file
-    #output_path = "file:///home/user/Downloads/task3.csv"  # Modifica il percorso del file di output
-    ## Scrivi il DataFrame in un file CSV senza sovrascrivere
-    #try:
-    #    df_top_10_stazioni.select("YEAR", "STATION", "avg_precipitation") \
-    #        .coalesce(1) \
-    #    .write \
-    #        .csv(output_path, header=True)
-    #    print(f"File salvato con successo in {output_path}")
-    #except Exception as e:
-    #    print(f"Errore nel salvataggio del file: {e}")
 
 # Main block
 if __name__ == '__main__':
     start_time = time.time()
-    # Read the CSV files
+    # Read all CSV files
     df = read_csv(ENTRY_POINT)
 
-    #task1(df)
-    #task2(df)
+    task1(df)
+    task2(df)
     task3(df)
-    end_time = time.time()  # Registra il tempo di fine
-    elapsed_time = end_time - start_time  # Calcola il tempo trascorso
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
     print("All operations have terminated in {:.2f} s.".format(elapsed_time))
